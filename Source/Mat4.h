42cff4ca6312d3783e4422871aa4f80c6cc6747d
Will Cassella <wcassella@gmail.com>
2014-02-03 18:53:49 -0700

    Update Mat4.h

--- Source/Mat4.h
+++ Source/Mat4.h
@@ -319,10 +319,10 @@ public:
 		const float depth = zMax - zMin;
 
 		return Mat4( 
-			2/width,	0,			0,			-(xMax+xMin)/width,
-			0,			2/height,	0,			-(yMax+yMin)/height,
-			0,			0,			-2/depth,	-(zMax+zMin)/depth,
-			0,			0,			0,			1 );
+			2/width,	0,		0,		-(xMax+xMin)/width,
+			0,		2/height,	0,		-(yMax+yMin)/height,
+			0,		0,		-2/depth,	-(zMax+zMin)/depth,
+			0,		0,		0,		1 );
 	}
 
 	// Returns a translation matrix from a 3-length vector
@@ -356,7 +356,7 @@ public:
 			1 - 2*y*y - 2*z*z,	2*x*y + 2*z*w,		2*x*z - 2*y*w,		0,
 			2*x*y - 2*z*w,		1 - 2*x*x - 2*z*z,	2*y*z + 2*x*w,		0,
 			2*x*z + 2*y*w,		2*y*z - 2*x*w,		1 - 2*x*x - 2*y*y,	0,
-			0,					0,					0,					1 );
+			0,			0,			0,			1 );
 	}
 
 	///////////////////////////////
